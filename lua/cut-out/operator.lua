e7d6a6edf3e77f588dfd280c203277d3d8326bcb
jhk <jhk06@mailbox.org>
2025-11-28 21:52:05 +0100

    feat: narrow highlighting when asking for name

--- lua/cut-out/operator.lua
+++ lua/cut-out/operator.lua
@@ -7,6 +7,17 @@ local hlns = api.nvim_create_namespace("cut-out")
 local config = require("cut-out").options
 local filetype = config.filetypes
 
+local highlight_matching = function(matching, group)
+    for _, n in ipairs(matching) do
+        local srow, scol, erow, ecol = n:range()
+        api.nvim_buf_set_extmark(0, hlns, srow, scol, {
+            end_row = erow,
+            end_col = ecol,
+            hl_group = group
+        })
+    end
+end
+
 local function get_mark(mark)
     return api.nvim_buf_get_mark(0, mark)
 end
@@ -36,6 +47,8 @@ local last_node
 local last_register
 ---@type Range4
 local last_range
+---@type integer?
+local last_aucmd
 
 ---@param range Range4
 local select_expr_to_cut = function(range)
@@ -48,42 +61,48 @@ local select_expr_to_cut = function(range)
     next_phase = "target"
     last_register = vim.v.register
     last_range = range
+
     local win_start, win_end = vim.fn.getpos("w0"), vim.fn.getpos("w$")
     local winrange = { win_start[2] - 1, win_start[3], win_end[2] - 1, win_end[3], }
+
     local containing_node = my_ts.node_for_range(parser, winrange, range)
     local possible_matches = my_ts.find_matching_inside_node(node, containing_node, winrange)
 
-    for _, n in ipairs(possible_matches) do
-        local srow, scol, erow, ecol = n:range()
-        api.nvim_buf_set_extmark(0, hlns, srow, scol, {
-            end_row = erow,
-            end_col = ecol,
-            hl_group = config.hl_group
-        })
-    end
+    highlight_matching(possible_matches, config.hl_group)
 
-    api.nvim_feedkeys("g@", "n", false)
-    api.nvim_create_autocmd("SafeState", {
+    last_aucmd = api.nvim_create_autocmd("SafeState", {
         once = true,
         callback = function()
             next_phase = "expression"
             api.nvim_buf_clear_namespace(0, hlns, 0, -1)
+            last_aucmd = nil
         end
     })
+
+    api.nvim_feedkeys("g@", "n", false)
 end
 
+---@param mode "line"|"char"
 ---@param range Range4
-local select_region = function(range)
+local select_region = function(mode, range)
     local parser = vim.treesitter.get_parser(0):language_for_range(last_range)
     if not last_node or not parser then
         return
     end
 
     -- force whole lines
-    range[1] = 0
-    range[4] = #api.nvim_buf_get_lines(0, range[3], range[3] + 1, false)[1]
+    if mode == "line" then
+        range[2] = 0
+        range[4] = #api.nvim_buf_get_lines(0, range[3], range[3] + 1, false)[1]
+    end
     local containing_node = my_ts.node_for_range(parser, range, last_range)
     local matching = my_ts.find_matching_inside_node(last_node, containing_node, range)
+    if last_aucmd then
+        api.nvim_del_autocmd(last_aucmd)
+        api.nvim_buf_clear_namespace(0, hlns, 0, -1)
+        last_aucmd = nil
+    end
+    highlight_matching(matching, config.hl_group)
 
     local ft = parser:lang()
     local for_ft = filetype[ft] or {}
@@ -112,6 +131,8 @@ local select_region = function(range)
         local replacement = replacer and replacer(name, last_node, node) or { name }
         api.nvim_buf_set_text(0, srow, scol, erow, ecol, replacement)
     end
+    next_phase = "expression"
+    api.nvim_buf_clear_namespace(0, hlns, 0, -1)
 end
 
 M.opfunc = function(mode)
@@ -124,7 +145,7 @@ M.opfunc = function(mode)
     if next_phase == "expression" then
         select_expr_to_cut(range)
     else
-        select_region(range)
+        select_region(mode, range)
     end
 end
 
